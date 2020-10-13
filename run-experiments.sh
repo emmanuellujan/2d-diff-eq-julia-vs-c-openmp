7053399d3346d73a1fbce0517c3a27f721816823
Emmanuel Lujan <lujan.emmanuel@gmail.com>
2020-10-13 04:50:22 -0300

    improving experiment parameter comments

--- run-experiments.sh
+++ run-experiments.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # Experiment parameters
-export NUM_MAX_THREADS=8
+export NUM_THREADS=8
 export NUM_MEASUREMENTS=10 #30
-export NUM_MAX_CONV_IT=100 #100000
+export NUM_CONVERGENCE_IT=100 #100000
 export NUM_MESH_NODES=6500000 #4000000
 
 # Clear data files and compile C code
@@ -16,14 +16,14 @@ echo "No. threads,Time" >> diffusion_parallel_test_c.dat
 
 for I in `seq 1 $NUM_MEASUREMENTS`;
 do
-    for NUM_THREADS in `seq 1 $NUM_MAX_THREADS`
+    for NUM_THREADS in `seq 1 $NUM_THREADS`
     do
         export JULIA_NUM_THREADS=$NUM_THREADS;
-        TIME=`julia diffusion_parallel_test.jl $NUM_MAX_CONV_IT $NUM_MESH_NODES`
+        TIME=`julia diffusion_parallel_test.jl $NUM_CONVERGENCE_IT $NUM_MESH_NODES`
         echo $NUM_THREADS,$TIME >> diffusion_parallel_test_julia.dat
 
         export OMP_NUM_THREADS=$NUM_THREADS;
-        TIME=`./diffusion_parallel_test $NUM_MAX_CONV_IT $NUM_MESH_NODES`
+        TIME=`./diffusion_parallel_test $NUM_CONVERGENCE_IT $NUM_MESH_NODES`
         echo $NUM_THREADS,$TIME >> diffusion_parallel_test_c.dat
     done
 done
